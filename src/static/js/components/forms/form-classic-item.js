78f713114884460e92ceb3c04c6f76a6d1d40bc1
Vladimir Barbaros <vladimir.barbarosh@gmail.com>
2025-09-20 13:42:54 +0300

    form-classic-item: remove value prop

--- src/static/js/components/forms/form-classic-item.js
+++ src/static/js/components/forms/form-classic-item.js
@@ -1,13 +1,13 @@
 vue_component('form-classic-item', {
-    props: ['value', 'item'],
+    props: ['item'],
     template: `
         <div v-if="(item.type === 'checkbox')" class="flex-row-center-left gap5">
-            <form-render-control v-bind:value="value" v-bind:item="item" />
-            <form-render-label v-bind:value="value" v-bind:item="item" />
+            <form-render-control v-bind:item="item" />
+            <form-render-label v-bind:item="item" />
         </div>
         <div v-else class="flex-col flex-align-stretch">
-            <form-render-label v-bind:value="value" v-bind:item="item" />
-            <form-render-control v-bind:value="value" v-bind:item="item" />
+            <form-render-label v-bind:item="item" />
+            <form-render-control v-bind:item="item" />
         </div>
     `,
 });
